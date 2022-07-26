cd6273f8245a8d08adb519aba68081b835dbdb71
ggghj gds <2057552446@qq.com>
2022-07-26 10:09:50 +0800

    mysql

--- backend/src/main/java/com/kob/backend/comtroller/pk/Hello.java
+++ backend/src/main/java/com/kob/backend/comtroller/pk/Hello.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pk/")
+
 public class Hello {
-    @RequestMapping("index/")
+    @RequestMapping("/")
     public String Index() {
         return "pk/hello.html";
     }
